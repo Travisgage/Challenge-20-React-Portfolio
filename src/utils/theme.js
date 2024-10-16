a2894aa0e40eb8d4dff5193420f628e21c0000d2
Travisgage <travisgagephillips@gmail.com>
2024-10-15 20:50:43 -0600

    Update theme.js

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,16 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
 import background from '../assets/images/background.png';
 import { Global } from "@emotion/react";
-import fonts from '../assets/vertopal.com_Mitashi-Tech.woff';
 
 const theme = extendTheme({
     colors: {
         brand: {
-            50: "#ccafcd", //pale lavender
-            200: "#9a8fab", //light lavender
-            300: "#e6d7e6", //off-white
-            500: "#3f3b58", //medium purple
-            800: "#202a35", //dark purple
+            50: "#ccafcd", 
+            200: "#9a8fab", 
+            300: "#e6d7e6", 
+            500: "#3f3b58", 
+            800: "#202a35", 
         }
     },
     styles: {
